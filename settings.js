fbe4d22e9aa2b7a73323015f8190ed63664088d6
Austin <eh.jay.cee@gmail.com>
2022-08-21 18:42:49 -0400

    'stuff'

---
+++ settings.js
@@ -0,0 +1,10 @@
+const settingsForm = document.getElementById('settings_form')
+const activeToggle = document.getElementById('active_toggle')
+
+function onActiveToggle() {
+
+}
+
+function onFormSave() {
+  
+}
